fca1eefcc37b103c030f2a2a12a8edf346fea96a
Marco Geri <m.geri1974@gmail.com>
2023-09-26 14:30:12 +0200

    Use official semantic version regex
    This commit allow to create full jira versions
    that follow official semantic versioning.

--- utils/jira-helper.js
+++ utils/jira-helper.js
@@ -50,7 +50,10 @@ function parseForWord(arr) {
  */
 function parseForVersion(version) {
   try {
-    const regex = /v\d+.\d+.\d+/g
+    // semantic version regex from https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+    // prefixed with "v"
+    const regex =
+        /^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/g
     version = [...version.matchAll(regex)]
   } catch (error) {
     core.setFailed(error.message)
